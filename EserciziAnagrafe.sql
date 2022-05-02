b5dc83dc19d0b7d5c06f374d36818cdef1974934
Selv98 <selvana.hosni@gmail.com>
2022-05-02 15:56:09 +0200

    Query 3

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -2,3 +2,8 @@
 select Persona.Nome, Città.Regione
 from Persona join Città on Persona.CittàNascita=Città.Nome
 where Persona.Età>=18
+
+/* query3 */
+select Città.Nome
+From Città join Persona on Persona.CittàNascita=Città.Nome 
+where Persona.Età>=50
