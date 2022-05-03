5a31c3b6a60e8f97d1af9e61646cbfcb69fb78bb
Selv98 <selvana.hosni@gmail.com>
2022-05-03 11:33:21 +0200

    Query 2

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -3,3 +3,10 @@ select Programmatore.codice
 from Autore join Programmatore on Autore.codice= Programmatore.codice
     join Programma  on Autore.id=Programma.id
 where Programma.linguaggio="Java" and Programma.anno>=2000
+
+/* Query 2 */
+select Programmatore.nome, Programmatore.categoria
+from Autore join Programmatore on Autore.codice= Programmatore.codice
+    join Programma  on Autore.id=Programma.id
+where Programma.linguaggio != "Python" 
+order by Programmatore.nome
