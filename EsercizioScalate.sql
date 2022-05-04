d7f2e32fd48f774aa87d2d2691abd0fdfdbcdc5f
Selv98 <selvana.hosni@gmail.com>
2022-05-04 12:55:28 +0200

    correzione query 1

--- EsercizioScalate.sql
+++ EsercizioScalate.sql
@@ -1,5 +1,5 @@
 /* Query 1 */
-select Scalata.Scalatore, Scalata.Nazione
+select distinct Scalata.Scalatore, Scalata.Nazione
 from Scalata join Scalatore on Scalata.Scalatore= Scalatore.CF
     join Nazione on Scalata.Nazione= Nazione.nome
 
