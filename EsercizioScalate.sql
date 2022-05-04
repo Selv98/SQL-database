6452e55604ebc25d10cd931234dcc05615109e04
Selv98 <selvana.hosni@gmail.com>
2022-05-04 11:52:49 +0200

    Query 2

--- EsercizioScalate.sql
+++ EsercizioScalate.sql
@@ -2,3 +2,11 @@
 select Scalata.Scalatore, Scalata.Nazione
 from Scalata join Scalatore on Scalata.Scalatore= Scalatore.CF
     join Nazione on Scalata.Nazione= Nazione.nome
+
+/* Query 2 */
+select distinct Scalatore.CF,Nazione.continente
+from Scalata join Scalatore on Scalata.Scalatore= Scalatore.CF
+    join Nazione on Scalata.Nazione= Nazione.nome
+where Scalatore.annoNascita<1980
+group by Scalatore.CF, Nazione.continente
+order by Nazione.continente
